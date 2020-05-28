7dd15aec768804e60f9fd15a72c9e6c841c2a9e1
Zoilo Mora <zoilo.mora@hotmail.com>
2020-05-28 11:29:04 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Elastic APM for Guzzle
+
+This library supports Span traces of HTTP calls and distributed traces with [Guzzle](https://github.com/guzzle/guzzle).
+
+## Usage
+
+In all cases, an already created instance of [ElasticApmTracer](https://github.com/zoilomora/elastic-apm-agent-php) is assumed.
+
+### Native PHP
+
+```php
+<?php
+declare(strict_types=1);
+
+$apmMiddleware = PcComponentes\ElasticAPM\GuzzleHttp\ElasticApmMiddleware::trace(
+    $apmTracer, /** \ZoiloMora\ElasticAPM\ElasticApmTracer instance. */
+);
+
+$handler = GuzzleHttp\HandlerStack::create();
+$handler->push($apmMiddleware);
+
+$config = [
+    'handler' => $handler,
+];
+
+$client = new GuzzleHttp\Client($config);
+```
+
+### Service Container (Symfony)
+
+```yaml
+http.client:
+  class: GuzzleHttp\Client
+  arguments:
+    $config:
+      handler: '@guzzle.handler'
+
+guzzle.handler:
+  class: GuzzleHttp\HandlerStack
+  factory: 'GuzzleHttp\HandlerStack::create'
+  calls:
+    - method: push
+      arguments: ['@guzzle.middleware.apm', 'trace']
+
+guzzle.middleware.apm:
+  class: Closure
+  factory: 'PcComponentes\ElasticAPM\GuzzleHttp\ElasticApmMiddleware::trace'
+  arguments:
+    $elasticApmTracer: '@apm.tracer' # \ZoiloMora\ElasticAPM\ElasticApmTracer instance.
+```
+
+## License
+Licensed under the [MIT license](http://opensource.org/licenses/MIT)
+
+Read [LICENSE](LICENSE) for more information
---
+++ src/GuzzleHttp/ElasticApmMiddleware.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types=1);
+
+namespace PcComponentes\ElasticAPM\GuzzleHttp;
+
+use GuzzleHttp\Psr7\Request;
+use ZoiloMora\ElasticAPM\ElasticApmTracer;
+use ZoiloMora\ElasticAPM\Events\Span\Context;
+use ZoiloMora\ElasticAPM\Helper\DistributedTracing;
+
+final class ElasticApmMiddleware
+{
+    private const STACKTRACE_SKIP = 11;
+
+    public static function trace(ElasticApmTracer $elasticApmTracer): \Closure
+    {
+        return static function (callable $handler) use ($elasticApmTracer) {
+            return static function (Request $request, array $options) use ($handler, $elasticApmTracer) {
+                $name = \sprintf(
+                    '%s %s',
+                    $request->getMethod(),
+                    (string) $request->getUri(),
+                );
+
+                $span = $elasticApmTracer->startSpan(
+                    $name,
+                    'request',
+                    null,
+                    null,
+                    new Context(),
+                    self::STACKTRACE_SKIP,
+                );
+
+                $newRequest = $request->withHeader(
+                    DistributedTracing::HEADER_NAME,
+                    $span->distributedTracing(),
+                );
+
+                /** @var \GuzzleHttp\Promise\FulfilledPromise $result */
+                $result = $handler($newRequest, $options);
+
+                /** @var \GuzzleHttp\Psr7\Response $response */
+                $response = $result->wait();
+
+                $span->stop();
+
+                $span->context()->setHttp(
+                    new Context\Http(
+                        (string) $request->getUri(),
+                        $response->getStatusCode(),
+                        $request->getMethod(),
+                    )
+                );
+
+                return $result;
+            };
+        };
+    }
+}
